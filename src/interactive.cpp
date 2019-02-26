5b25425b73ab6a19dff0cc12b6c6277ab59b3e30
Benjamin Gallois <gallois.benjamin08@gmail.com>
2019-02-26 15:19:12 +0100

    [feat] interactive: add mouse crop

--- src/interactive.cpp
+++ src/interactive.cpp
@@ -1,31 +1,114 @@
+/*
+This file is part of Fast Track.
+
+    FastTrack is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    FastTrack is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FastTrack.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+
 #include "interactive.h"
 #include "ui_interactive.h"
 
+/**
+ * @class Interactive
+ *
+ * @brief The Interactive widget provides an environment to use the FastTrack Tracking widget in an interactive environment.
+ *
+ * @author Benjamin Gallois
+ *
+ * @version $Revision: 4.1 $
+ *
+ * Contact: gallois.benjamin08@gmail.com
+ *
+ */
+
+
+
+/**
+  * @brief Constructs the interactive object.
+*/
 Interactive::Interactive(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::Interactive)
 {
     ui->setupUi(this);
 
+    // Menu bar
     connect(ui->actionOpen, &QAction::triggered, this, &Interactive::openFolder);
     connect(ui->slider, &QSlider::valueChanged, this, static_cast<void (Interactive::*)(int)>(&Interactive::display));
+    
+    // Threshold slider and combobox linking
     connect(ui->threshSlider, &QSlider::valueChanged, ui->threshBox, static_cast<void (QSpinBox::*)(int)>(&QSpinBox::setValue));
     connect(ui->threshBox, static_cast<void (QSpinBox::*)(int)>(&QSpinBox::valueChanged), ui->threshSlider, &QSlider::setValue);
     connect(ui->threshBox, static_cast<void (QSpinBox::*)(int)>(&QSpinBox::valueChanged), [this]() {
-        display(ui->slider->value());
+      ui->isBin->setChecked(true);
+      display(ui->slider->value());
     });
 
-    connect(ui->computeBack, &QPushButton::clicked, this, &Interactive::computeBackground);
-    connect(ui->preview, &QPushButton::clicked, this, &Interactive::previewTracking); 
+    // Sets the roi limits
+    connect(ui->x2, static_cast<void (QSpinBox::*)(int)>(&QSpinBox::valueChanged), [this](int value) {
+      ui->x1->setMaximum(value - 1);
+      ui->x1->setMinimum(0);
+    });
+    connect(ui->y2, static_cast<void (QSpinBox::*)(int)>(&QSpinBox::valueChanged), [this](int value) {
+      ui->y1->setMaximum(value - 1);
+      ui->y1->setMinimum(0);
+    });
+
+    // Morphological operations updat
+    connect(ui->erosion, static_cast<void (QSpinBox::*)(int)>(&QSpinBox::valueChanged), [this]() {
+      display(ui->slider->value());
+    });
+    connect(ui->dilatation, static_cast<void (QSpinBox::*)(int)>(&QSpinBox::valueChanged), [this]() {
+      display(ui->slider->value());
+    });
+    
+    // Ui buttons connects
+    connect(ui->backgroundSelectButton, &QPushButton::clicked, this, &Interactive::selectBackground);
+    connect(ui->backgroundComputeButton, &QPushButton::clicked, this, &Interactive::computeBackground);
+    connect(ui->previewButton, &QPushButton::clicked, this, &Interactive::previewTracking); 
+    connect(ui->trackButton, &QPushButton::clicked, [this]() {
+      QMessageBox::StandardButton reply;
+      reply = QMessageBox::question(this, "Confirmation", "You are going to start a full tracking analysis. That can be take some time, are you sure?", QMessageBox::Yes|QMessageBox::No);
+      if (reply == QMessageBox::Yes) {
+        track();
+      }
+    });
+    connect(ui->cropButton, &QPushButton::clicked, this, &Interactive::crop); 
+    connect(ui->resetButton, &QPushButton::clicked, this, &Interactive::reset); 
+    
+    
     isBackground = false;
     tracking = new Tracking("", "");
 
+    colorMap;
+    double a,b,c;
+    srand (time(NULL));
+    for (int j = 0; j < 500 ; ++j)  {
+      a = rand() % 255;
+      b = rand() % 255;
+      c = rand() % 255;
+      colorMap.push_back(Point3f(a, b, c));
+    }
 }
 
 
+/**
+  * @brief Gets the path to a folder where an image sequence is stored. Triggered when open button from menu bar is clicked.
+*/
 void Interactive::openFolder() {
     
-    QString dir = QFileDialog::getExistingDirectory(this, tr("Open Directory"), memoryDir, QFileDialog::ShowDirsOnly);
+    dir = QFileDialog::getExistingDirectory(this, tr("Open Directory"), memoryDir, QFileDialog::ShowDirsOnly);
     
     if (dir.length()) {
 
@@ -43,6 +126,7 @@ void Interactive::openFolder() {
           // Gets the paths to all the frames in the folder and puts it in a vector.
           // Setups the ui by setting maximum and minimum of the slider bar.
           string path = (dir + QDir::separator() + "*." + extension).toStdString();
+          memoryDir = dir;
           glob(path, framePath, false); // Gets all the paths to frames
           ui->slider->setMinimum(0);
           ui->slider->setMaximum(framePath.size() - 1);
@@ -50,6 +134,8 @@ void Interactive::openFolder() {
           Mat frame = imread(framePath.at(0), IMREAD_COLOR);
           originalImageSize.setWidth(frame.cols);
           originalImageSize.setHeight(frame.rows);
+          ui->x2->setMaximum(frame.cols);
+          ui->y2->setMaximum(frame.rows);
           isBackground = false;
           path = dir.toStdString();
           display(0);
@@ -59,6 +145,10 @@ void Interactive::openFolder() {
     }
 }
 
+
+/**
+  * @brief Displays the image at index in the image sequence in the ui->display..
+*/
 void Interactive::display(int index) {
   
   if ( !framePath.empty() ) {
@@ -76,18 +166,36 @@ void Interactive::display(int index) {
     if ( ui->isBin->isChecked() && isBackground ) {
       (ui->backColor->currentText() == "Light background") ? (subtract(background, image, image)) : (subtract(image, background, image));
       tracking->binarisation(image, 'b', ui->threshBox->value());
+      Mat element = getStructuringElement( MORPH_ELLIPSE, Size( 2*ui->dilatation->value() + 1, 2*ui->dilatation->value() + 1 ), Point( ui->dilatation->value(), ui->dilatation->value() ) );
+      dilate(image, image, element);
+      element = getStructuringElement( MORPH_ELLIPSE, Size( 2*ui->erosion->value() + 1, 2*ui->erosion->value() + 1 ), Point( ui->erosion->value(), ui->erosion->value() ) );
+      erode(image, image, element);
     }
 
-    if ( ui->isTracked->isChecked() && !buffer.isEmpty() ) {
-      //To do
-    }
 
     // Displays the image in the QLabel
     Mat frame = image.getMat(ACCESS_READ);
     cvtColor(frame, frame, COLOR_GRAY2RGB);  
+    if ( ui->isTracked->isChecked() ) {
+      Data *trackingData = new Data(dir);
+      QList<int> idList = trackingData->getId(index);
+        
+      double scale = 2;
+      
+      for (auto const &a: idList) {
+
+        QMap<QString, double> coordinate = trackingData->getData(index, a);
+        int id = a;
+        cv::ellipse(frame, Point( coordinate.value("xHead"), coordinate.value("yHead") ), Size( coordinate.value("headMajorAxisLength"), coordinate.value("headMinorAxisLength") ), 180 - (coordinate.value("tHead")*180)/M_PI, 0, 360,  Scalar(colorMap.at(id).x, colorMap.at(id).y, colorMap.at(id).z), scale, 8 );
+        cv::ellipse(frame, Point( coordinate.value("xTail"), coordinate.value("yTail") ), Size( coordinate.value("tailMajorAxisLength"), coordinate.value("tailMinorAxisLength") ), 180 - (coordinate.value("tTail")*180)/M_PI, 0, 360,  Scalar(colorMap.at(id).x, colorMap.at(id).y, colorMap.at(id).z), scale, cv::LINE_AA );
+      }
+    }
+    if(roi.width != 0 || roi.height != 0 ) {
+      frame = frame(roi);
+    }
     double w = ui->display->width();
     double h = ui->display->height();
-    QPixmap resizedPix = (QPixmap::fromImage(QImage(frame.data, frame.cols, frame.rows, frame.step, QImage::Format_RGB888)).scaled(w, h, Qt::KeepAspectRatio));
+    resizedPix = (QPixmap::fromImage(QImage(frame.data, frame.cols, frame.rows, frame.step, QImage::Format_RGB888)).scaled(w, h, Qt::KeepAspectRatio));
     ui->display->setPixmap(resizedPix);
     resizedFrame.setWidth(resizedPix.width());
     resizedFrame.setHeight(resizedPix.height());
@@ -95,13 +203,16 @@ void Interactive::display(int index) {
   
 }
 
+/**
+  * @brief This is an overloaded function to display an image UMat in the ui->display.
+*/
 void Interactive::display(UMat image) {
 
     cvtColor(image, image, COLOR_GRAY2RGB);  
     Mat frame = image.getMat(ACCESS_READ);
     double w = ui->display->width();
     double h = ui->display->height();
-    QPixmap resizedPix = (QPixmap::fromImage(QImage(frame.data, frame.cols, frame.rows, frame.step, QImage::Format_RGB888)).scaled(w, h, Qt::KeepAspectRatio));
+    resizedPix = (QPixmap::fromImage(QImage(frame.data, frame.cols, frame.rows, frame.step, QImage::Format_RGB888)).scaled(w, h, Qt::KeepAspectRatio));
     ui->display->setPixmap(resizedPix);
     resizedFrame.setWidth(resizedPix.width());
     resizedFrame.setHeight(resizedPix.height());
@@ -109,18 +220,42 @@ void Interactive::display(UMat image) {
 }
 
 
+/**
+  * @brief Computes and displays the background image in the ui->display. Triggered when ui->backgroundComputeButton is clicked.
+*/
 void Interactive::computeBackground() {
- 
- if ( !framePath.empty() ) { 
+
+  if ( !framePath.empty() ) { 
     double nBack = double(ui->nBack->value());
     int method = ui->back->currentIndex();
     background = tracking->backgroundExtraction(framePath, nBack, method); 
     display(background);
     isBackground = true;
  }
+ else {
+    imread(backgroundPath.toStdString(), IMREAD_GRAYSCALE).copyTo(background);
+ }
 
 }
 
+
+/**
+  * @brief Opens a dialogue to select a background image. Triggered when ui->backgroundSelectButton is pressed.
+*/
+void Interactive::selectBackground() {
+    
+  dir = QFileDialog::getExistingDirectory(this, tr("Open Background Image"), memoryDir);
+    
+  if (dir.length()) {
+    backgroundPath = dir;
+  }
+}
+
+
+
+/**
+  * @brief Gets all the tracking parameters from the ui and updates the parameter map.
+*/
 void Interactive::getParameters() {
 
   parameters.insert("Maximal size", QString::number(ui->maxSize->value()));
@@ -134,6 +269,7 @@ void Interactive::getParameters() {
 
   parameters.insert("Binary threshold", QString::number(ui->threshBox->value()));
   parameters.insert("Number of images background", QString::number(ui->nBack->value()));
+  parameters.insert("Background method", QString::number(ui->back->currentIndex()));
   parameters.insert("ROI top x", QString::number(ui->x1->value()));
   parameters.insert("ROI top y", QString::number(ui->y1->value()));
   parameters.insert("ROI bottom x", QString::number(ui->x2->value()));
@@ -145,27 +281,131 @@ void Interactive::getParameters() {
 
 }
 
+
+/**
+  * @brief Does a preview of a tracking analysis. Triggered when previewButton is clicked.
+*/
 void Interactive::previewTracking() {
-  buffer.clear();
-  QThread *thread = new QThread;
-  Tracking *tracking = new Tracking(path.toStdString(), backgroundPath.toStdString(), ui->startImage->value(), ui->startImage->value() + ui->stopImage->value());
-  tracking->moveToThread(thread);
+  if(!framePath.empty()) {
+    QThread *thread = new QThread;
+    Tracking *tracking = new Tracking((dir + QDir::separator()).toStdString(), backgroundPath.toStdString(), ui->startImage->value(), ui->startImage->value() + ui->stopImage->value());
+    tracking->moveToThread(thread);
 
-  connect(tracking, &Tracking::finished, thread, &QThread::quit);
-  connect(tracking, &Tracking::finished, tracking, &Tracking::deleteLater);
-  connect(thread, &QThread::finished, thread, &QThread::deleteLater);
-  connect(tracking, &Tracking::newImageToDisplay, [this](UMat& binary, UMat& visu) {
-    buffer.append(visu);
-  });
+    connect(thread, &QThread::started, tracking, &Tracking::startProcess);
+    connect(tracking, &Tracking::finished, thread, &QThread::quit);
+    connect(tracking, &Tracking::finished, tracking, &Tracking::deleteLater);
+    connect(thread, &QThread::finished, thread, &QThread::deleteLater);
 
-  tracking->updatingParameters(parameters);
-  thread->start();
+    getParameters();
+    tracking->updatingParameters(parameters);
+    thread->start();
+    ui->isTracked->setChecked(true);
+  }
 
 }
 
 
-Interactive::~Interactive()
-{
+/**
+  * @brief Does a tracking analysis. Triggered when previewButton is clicked.
+*/
+void Interactive::track() {
+  if(!framePath.empty()) {
+    QThread *thread = new QThread;
+    Tracking *tracking = new Tracking((dir + QDir::separator()).toStdString(), backgroundPath.toStdString());
+    tracking->moveToThread(thread);
+
+    connect(thread, &QThread::started, tracking, &Tracking::startProcess);
+    connect(tracking, &Tracking::finished, thread, &QThread::quit);
+    connect(tracking, &Tracking::finished, tracking, &Tracking::deleteLater);
+    connect(thread, &QThread::finished, thread, &QThread::deleteLater);
+    connect(tracking, &Tracking::newImageToDisplay, [this](UMat& binary, UMat& visu) {
+    });
+
+    getParameters();
+    tracking->updatingParameters(parameters);
+    thread->start();
+    ui->isTracked->setChecked(true);
+  }
+}
+
+
+/**
+  * @brief Gets the mouse coordinate in the frame of reference of the widget where user have clicked.
+  * @param[in] event Describes the mouse event.
+*/
+void Interactive::mousePressEvent(QMouseEvent* event) {
+
+    if (event->buttons() == Qt::LeftButton) {
+      clicks.first = ui->display->mapFrom(this, event->pos()); 
+      
+      // The QPixmap is V/Hcentered in the Qlabel widget
+      // Gets the click coordinate in the frame of reference of the centered display
+      clicks.first.setX(clicks.first.x() -  0.5*(ui->display->width() - resizedFrame.width())); 
+      clicks.first.setY(clicks.first.y() - 0.5*(ui->display->height() - resizedFrame.height()));
+    }
+}
+
+
+/**
+  * @brief Draws a rectangle when the mouse is pressed in a QPixmap. The top left corner is at the coordinate of the first click and the bottom right at the coordinate of the released.
+  * @param[in] event Describes the mouse event.
+*/
+void Interactive::mouseMoveEvent(QMouseEvent* event) {
+   
+      clicks.second = (ui->display->mapFrom(this, event->pos())); 
+      
+      // The QPixmap is V/Hcentered in the Qlabel widget
+      // Gets the click coordinate in the frame of reference of the centered display
+      clicks.second.setX(clicks.second.x() -  0.5*(ui->display->width() - resizedFrame.width())); 
+      clicks.second.setY(clicks.second.y() - 0.5*(ui->display->height() - resizedFrame.height()));
+      
+      QPixmap tmpImage = resizedPix;
+      QPainter paint(&tmpImage);
+      paint.setPen(QColor(0, 230, 0, 255));
+      paint.drawRect(QRect(clicks.first.x(), clicks.first.y(), clicks.second.x() - clicks.first.x(), clicks.second.y() - clicks.first.y())); 
+      ui->display->setPixmap(tmpImage);
+
+      // Updates ui value
+      ui->x1->setValue(clicks.first.x());
+      ui->y1->setValue(clicks.first.y()); 
+      ui->x2->setValue(clicks.second.x());
+      ui->y2->setValue(clicks.second.y()); 
+}
+
+ /**
+   * @brief Crops the image from a rectangle drawed with the mouse on the ui->display. Triggered when the QPushButton ui->crop is clicked.
+ */
+void Interactive::crop() {
+ 
+  // Converts clicks from display widget frame of reference to original image frame of reference 
+  double xTop = double(clicks.first.x())*double(originalImageSize.height()) / double(resizedFrame.height()) + roi.tl().x ;
+  double yTop = double(clicks.first.y())*double(originalImageSize.height()) / double(resizedFrame.height()) + roi.tl().y;
+  double xBottom = double(clicks.second.x())*double(originalImageSize.height()) / double(resizedFrame.height()) + roi.tl().x;
+  double yBottom = double(clicks.second.y())*double(originalImageSize.height()) / double(resizedFrame.height()) + roi.tl().y;
+  roi = Rect(xTop, yTop, xBottom - xTop, yBottom - yTop);
+  originalImageSize.setWidth(roi.width);
+  originalImageSize.setHeight(roi.height);
+  display(ui->slider->value());
+ }
+ 
+
+
+ /**
+   * @brief Reset the region of interest. Triggered by the ui->reset
+ */
+void Interactive::reset() {
+  ui->x1->setValue(0);
+  ui->y1->setValue(0); 
+  ui->x2->setValue(originalImageSize.width());
+  ui->y2->setValue(originalImageSize.height()); 
+  roi = Rect(0, 0, originalImageSize.width(), originalImageSize.height());
+  display(ui->slider->value());
+ }
+
+/**
+  * @brief Destructors.
+*/
+Interactive::~Interactive() {
     delete tracking;
     delete ui;
 }
--- src/interactive.h
+++ src/interactive.h
@@ -1,15 +1,36 @@
+/*
+This file is part of Fast Track.
+
+    FastTrack is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    FastTrack is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FastTrack.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+
 #ifndef INTERACTIVE_H
 #define INTERACTIVE_H
 
 #include <QMainWindow>
 #include <QFileDialog>
+#include <QMessageBox>
 #include <QAction>
 #include <QMap>
 #include <QString>
 #include <QDirIterator>
+#include <QPainter>
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/highgui/highgui.hpp"
 #include "tracking.h"
+#include "data.h"
 
 
 using namespace std;
@@ -29,29 +50,44 @@ public:
 
 private slots:
     void openFolder();
+   
     void display(int index);
     void display(UMat image);
+
     void getParameters();
+
     void previewTracking();
+    void track();
 
 
     void computeBackground();
+    void selectBackground();
 
+    void mouseMoveEvent(QMouseEvent* event);
+    void mousePressEvent(QMouseEvent* event);
+    void crop();
+    void reset();
 
 private:
     Ui::Interactive *ui;
-    QString memoryDir;
-    vector<String> framePath;
-    QSize resizedFrame;
-    QSize originalImageSize;
-    QMap<QString, QString> parameters;
-    QString path;
-    QString backgroundPath;
+    QString memoryDir;   /*!< Saves the path to the last opened folder in dialog. */
+    vector<String> framePath;   /*!< Path to all the image of the image sequence to display. */
+    QSize resizedFrame;   /*!< Size of the resized image in the display QWidget. */
+    QSize originalImageSize;   /*!< Size of the originla image. */
+    QMap<QString, QString> parameters;   /*!< Tracking parameters. */
+    QString path;   /*!< */
+    QString backgroundPath;   /*!< Path to the background image. */
+    QString dir;   /*!< Path to the folder where the image sequence to display is stored. */
+    Tracking *tracking;   /*!< Tracking object. */
+    UMat background;   /*!< Background image. */
+    bool isBackground;   /*!< Is the background computed. */
 
-    Tracking *tracking;
-    UMat background;
-    bool isBackground;
-    QVector<UMat> buffer;
+    QPair<QPoint, QPoint> clicks;
+
+    Rect roi;
+    QPixmap resizedPix;
+    
+    vector<Point3f> colorMap;
 };
 
 #endif // INTERACTIVE_H
